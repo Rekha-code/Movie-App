36893a9c1cb4f2372da60fff6ac600f6dc06119b
rekha_gorde <rekha.gorde8@gmail.com>
2024-04-14 15:00:12 +0530

    Movie-App

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+//import "./App.css";
+import MainMenu from "./Components/MainMenu";
+import "./Components/style.css";
+import Card from "./Components/Card";
+function App() {
+  return (
+    <>
+      <MainMenu></MainMenu>
+    </>
+  );
+}
+
+export default App;
